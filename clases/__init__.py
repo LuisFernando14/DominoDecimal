d332e7ab919868ccf726f05064ed883d5dfcc131
Luis Martinez <lfmartinezb@concredito.com.mx>
2017-10-06 18:30:16 -0600

    Puros comentarios

--- clases/__init__.py
+++ clases/__init__.py
@@ -4,10 +4,11 @@ if __name__ == '__main__':
     print "Hola"
     fichasJugador1 = []
     fichasJugador2 = []
-    tablero = [] #En teoría solo acepta unas 4 posiciones
+    tablero = [] # En teoría solo acepta unas 4 posiciones y no será una lista de listas
     puntajeJugador1 = 0;
     puntajeJugador2 = 0;
     condicion = True
+    movimeintoJugador = True
 
     fichas = [
         [0,0], [0,1], [0,2], [0,3], [0,4], [0,5], [0,6],
@@ -18,7 +19,7 @@ if __name__ == '__main__':
 
     for ficha in fichas:
         ficha[1]+=1
-        #print ficha[1]
+        # print ficha[1]
 
     def repartirFichas (fichas, jugador):
         for i in range (5):
@@ -44,10 +45,21 @@ if __name__ == '__main__':
     if(fichasJugador1.__contains__([5,5])): #empieza el jugador uno
         tablero.append([5,5])
         fichasJugador1.remove([5,5])
+        tablero.append([5,5])
+        movimeintoJugador = False
     elif (fichasJugador2.__contains__([5,5])):
         tablero.append([5, 5])
         fichasJugador2.remove([5, 5])
+        tablero.append([5, 5])
     #Aquí ya empieza el juego al cien
     while (len(fichasJugador1) != 0 and len(fichasJugador2) != 0) or condicion:
-        
+        if(movimeintoJugador): #jugador 1
+            #Verificar si puede hacer un movimiento
+            #verificar si puede tomar de la sobra
+            # Buscar fichas en la banca (los extremos) para ver cuál podemos poner
+            # poner la ficha
+            #No hacer nada y ceder el movimiento
+            pass
+        else: #jugador 2
+            pass
         pass
